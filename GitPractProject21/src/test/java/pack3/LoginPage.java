267585c3b3237bf5542eb20c5932c8976b1d94c4
hp <hp@DESKTOP-0H72QO4>
2026-09-21 05:19:28 -0700

    PPPPPPPP

--- GitPractProject21/src/test/java/pack3/LoginPage.java
+++ GitPractProject21/src/test/java/pack3/LoginPage.java
@@ -6,7 +6,7 @@ public class LoginPage {
 		System.out.println("----");
 		System.out.println("Login page is created");
         System.out.println("gitconflict creation");
-      
+      System.out.println("PPPPPPPP");
         
 	}
 
