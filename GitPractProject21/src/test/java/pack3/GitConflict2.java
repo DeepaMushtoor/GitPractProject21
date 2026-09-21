48bedc1ef2e7d036d315cec3335a28ed31acbae5
Kavya E <Kavya E@LAPTOP-IVE3LSR6>
2026-09-21 17:35:03 +0530

    Resolved

--- GitPractProject21/src/test/java/pack3/GitConflict2.java
+++ GitPractProject21/src/test/java/pack3/GitConflict2.java
@@ -3,12 +3,8 @@ package pack3;
 public class GitConflict2 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("execute conflict kavya");
-=======
-		System.out.println("priyanka");
-	
->>>>>>> branch 'master' of https://github.com/DeepaMushtoor/GitPractProject21.git
+
 
 	}
 
